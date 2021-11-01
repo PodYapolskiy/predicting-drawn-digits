6c824bfdd16fb0ba3abc89d244f4253719e0b528
Anatoly <podyapolskiyaa@gmail.com>
2021-11-01 13:48:59 +0300

    Solve problem with heavy dataset

--- nn.py
+++ nn.py
@@ -14,7 +14,7 @@ from sklearn.neural_network import MLPClassifier
     import h5py
     https://blog.stroganov.pro/%D1%81%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D0%B5%D1%81%D0%BE%D0%B2-%D0%BD%D0%B5%D0%B9%D1%80%D0%BE%D1%81%D0%B5%D1%82%D0%B8/
 '''
-df = pd.read_csv('mnist_784.csv')
+df = pd.read_csv('datasets/mnist_784.csv')
 
 x = df.loc[:, "pixel1":"pixel784"].values  # Все картинки со значениями пикселей от 0 до 256
 y = df["class"].values  # Все правильные ответы, которые должна предсказать нейронка
