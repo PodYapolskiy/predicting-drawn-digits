795643982327b4f34652882a7d85fb171db58a09
Anatoly <podyapolskiyaa@gmail.com>
2021-11-03 22:48:50 +0300

    Add some trained models

--- nn.py
+++ nn.py
@@ -1,45 +1,50 @@
 """Нейросеть, определяющая рукописное число.
-
-Keyword arguments:
-argument -- description
-Return: return_description
 """
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 import pickle
 
 
-model: "MLPClassifier" = pickle.load(open("models/model.sav", "rb"))
-
-
 def main():
     df = pd.read_csv('datasets/mnist_784.csv')
 
-    x = df.loc[:, "pixel1":"pixel784"].values  # Все картинки со значениями пикселей от 0 до 256
-    y = df["class"].values  # Все правильные ответы, которые должна предсказать нейронка
+    # Все картинки со значениями пикселей от 0 до 256
+    x = df.loc[:, "pixel1":"pixel784"].values
+    # Все правильные ответы, которые должна предсказать нейронка
+    y = df["class"].values
 
     assert len(x[0]) == 28 * 28
     assert x.shape == (70000, 784)
-    print()
-    first_picture = x[0]
-    # print(first_picture)
 
     # Распределяем тренировочную и тестовую выборку
     x_train, x_test = x[:60000], x[60000:]
     y_train, y_test = y[:60000], y[60000:]
 
-    # Наша модель - 3-слойный перцептрон
-    mlp = MLPClassifier(hidden_layer_sizes=(16, 16), verbose=True)
+    hidden_layers = [100, 100, 50]
+    max_iter = 150
+
+    # Наша модель - многослойный перцептрон
+    mlp = MLPClassifier(
+        hidden_layer_sizes=tuple(hidden_layers),
+        max_iter=max_iter,
+        verbose=True,
+        # alpha=0.001
+    )
     mlp.fit(x_train, y_train)
 
-    pickle.dump(mlp, open("models/model_1.sav", "wb"))
+    print(mlp.predict([x[0]]))  # 5
+    accuracy = mlp.score(x_test, y_test)
+    print(accuracy)
 
-    print(mlp.predict([x[0]]))
-    print(mlp.score(x_test, y_test))
-    # model: "MLPClassifier" = pickle.load(open("models/model.sav", "rb"))
-    # print(model.predict([x[0]]))
+    pickle.dump(
+        mlp,
+        open(
+            f"models/model a({accuracy:.3}) hl({'_'.join([str(i) for i in hidden_layers])}) i({max_iter}).sav",
+            "wb"
+        )
+    )
 
 
-# Если запустить файл на прямую будет происходить весь процесс формирования нейронной сети от загрузки данных до её обучения 
+# Если запустить файл на прямую будет происходить весь процесс формирования нейронной сети от загрузки данных до её обучения
 if __name__ == "__main__":
     main()
